d3475a3d6f5d8ab24acbea8c2c9ce5fec93e062f
Luis Chang <luis.chang@outlook.com>
2025-11-21 18:45:51 -0500

    Implement ProductItem component and update ProductList to display products

---
+++ src/components/product/ProductItem.vue
@@ -0,0 +1,37 @@
+<template>
+    <q-card class ="product-card">
+        <q-card-section>
+            <q-img :src="product.imageUrl" :alt="product.name" />            
+        </q-card-section>
+        <q-card-section>            
+            <div class="text-subtitle2 text-grey">{{ product.description }}</div>
+            <div class="text-h6 text-primary q-mt-sm">Precio: {{ product.price }}</div>
+            <q-btn label="Add to Cart" color="primary" class="full-width q-mt-md" />
+        </q-card-section>
+    </q-card>
+
+</template>
+
+<style>
+    .product-card {
+        max-width: 300px;
+        margin: auto;
+    }
+    .product-card q-card-section {
+        height: 100px;
+        overflow: hidden;
+    }
+</style>
+
+<script>
+export default {
+  name: "ProductItem",
+  props: {
+    product: {
+      type: Object,
+      required: true,
+    },
+  },
+};
+
+</script>
--- src/components/product/ProductList.vue
+++ src/components/product/ProductList.vue
@@ -1,7 +1,48 @@
 <template>
-  <h1>Acá se listarán los productos</h1>
+  <h6>Listado de Productos</h6>
+  <div class="product-list">
+    <div class = "product-grid">
+      <div class="product-item" v-for="item in products" :key="item.id">
+        <ProductItem :product="item"  />
+      </div>
+    </div>
+  </div>
 </template>
-
-<style></style>
+<style>
+.product-grid{
+  display: grid;
+  grid-template-columns: repeat(3, 1fr);
+  gap: 16px;
+}
+</style>
 <script>
+import ProductItem from "src/components/product/ProductItem.vue";
+export default {
+  name: "ProductList",
+  components: {
+    ProductItem,    
+  },
+  data() {
+    return {
+      products: [],
+    };
+  },  mounted() {
+    this.loadProducts();
+  },
+  methods: {
+    loadProducts() {
+      // Lógica para cargar productos desde una API o fuente de datos
+      let endpoint = "/api/product";
+      let token = JSON.parse(localStorage.getItem("token"));
+      this.$api.get(endpoint, {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      }).then((response) => {
+        //console.log(response.data);
+        this.products = response.data;
+      });
+    },
+  },
+};
 </script>
